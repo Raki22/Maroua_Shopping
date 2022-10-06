6212d2f0b30349044b20b95f6a08b92593694394
HibaSetti <hiba.ouldsetti@gmail.com>
2022-10-07 00:20:52 +0100

    slogan component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,12 @@
 <script>
-  import Navbar from './Navbar.vue'
+  import Navbar from './Navbar.vue';
+  import Slogan from './Slogan.vue'
   
   export default {
     name: 'Header',
     components: {
-      Navbar
+      Navbar,
+      Slogan
     },
   
   }
@@ -13,18 +15,16 @@
 <template>
   <div class="section1">
     <Navbar />
-  <div class="container">
-    <h1>Tagline brief simple wht compani its all about</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-    
-    </div>
+    <Slogan title="Tagline brief simple wht company its all about"
+            caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    />
   </div>
 </template>
 
 <style scoped>
 .section1 {
   width: 100%;
-  height: 600px;
+  height: 620px;
   background-image: url('@/assets/header-bg.png');
   background-size: cover;
 }
---
+++ src/components/Slogan.vue
@@ -0,0 +1,36 @@
+<script>
+    
+    export default {
+      name: 'Slogan',
+      props: {
+        title: String,
+        caption: String,
+    },
+    
+    }
+    </script>
+  
+  <template>
+    <div class="container row">
+    <div class="col-md-9 section">
+        <h1>{{title}}</h1>
+        <p>{{caption}}</p>
+        <button type="button" class="btn btn-primary">Explore Marwa</button>
+    </div>
+</div>
+  </template>
+  
+  <style scoped>
+h1 {
+    font-size: 61px;
+}
+p {
+    font-size: 20px;
+}
+.section {
+    color: white;
+    margin-top: 9%;
+    margin-left: 5%;
+}
+  </style>
+  
