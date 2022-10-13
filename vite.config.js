af51b787cec3c241afd2c7cd0b40a312c68aff3f
HibaSetti <hiba.ouldsetti@gmail.com>
2022-10-13 15:13:08 +0100

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-vite-project-Marouashopping/',
+  base: '/Maroua-shopping/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
